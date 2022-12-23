c658a81d120f14cff7170487a5587d7745c21d9e
Rahul राहुल <rahul_yadav@external.mckinsey.com>
2022-12-23 12:52:45 +0530

    Add a lot of snippets

--- tests/geoloc2.spec.ts
+++ tests/geoloc2.spec.ts
@@ -1,7 +1,7 @@
 import { chromium,test } from "@playwright/test";
 
 test.use({ viewport: { width: 1400, height: 1000 } });
-test('Launch blinkit page and set a location',async()=>{
+test('Open Lululemon webstie and set the location',async()=>{
 
    
     const browser = await chromium.launch({
@@ -13,18 +13,20 @@ test('Launch blinkit page and set a location',async()=>{
     const page = await context.newPage();
     
 
-    await page.goto("https://blinkit.com");
+    await page.goto("https://shop.lululemon.com/");
     await page.waitForTimeout(5000);
 
-    await page.getByText("Detect my location").click();
+     //await page.getByText('Store Locator').click();
+     //await page.locator('li:has-text("Store Locator") >> role=link[name="Store Locator"]').click();
+    await page.getByTestId('desktop-hnsl').click();
 
-    await context.setGeolocation({ longitude: 28.5311541, latitude: 77.3899336 });
+    await context.setGeolocation({ longitude: 40.739957, latitude: -73.792237 });
 
     await console.log('Location is set');
 
     await page.waitForTimeout(5000);
 
 
-  
+    
 });
 
