c96eb3c63aec1f0cede716b8f11cf2e8067a79e0
Rahul राहुल <rahul_yadav@external.mckinsey.com>
2023-01-18 11:02:18 +0530

    CSS scenarios

---
+++ tests/csss_attr.spec.ts
@@ -0,0 +1,34 @@
+import { chromium, expect, test } from "@playwright/test";
+
+test.use({ viewport: { width: 1400, height: 1000 } });
+test("Launch the Selectors hub test page", async () => {
+  const browser = await chromium.launch({
+    headless: false,
+  });
+  const context = await browser.newContext();
+  const page = await context.newPage();
+
+  await page.goto("https://selectorshub.com/xpath-practice-page/");
+  await page.waitForSelector(".dropbtn", {
+    state: "visible",
+  });
+
+  // we want to mask this locator
+  const btn = await page.locator(".dropbtn");
+  const color = await btn.evaluate((element) =>
+    window.getComputedStyle(element).getPropertyValue("background-color")
+  );
+
+  await console.log(`${color}`);
+
+  const text = await page.locator("a[href='https://www.youtube.com/c/SelectorsHub?sub_confirmation=1'] >> nth=0");
+  const textprop = await text.evaluate((element) =>
+    window.getComputedStyle(element).getPropertyValue("font-size")
+  );
+
+  await console.log(`${textprop}`);
+
+await expect(text).toHaveCSS('font-size', '16px');
+
+  
+});
--- tests/svgelements.spec.ts
+++ tests/svgelements.spec.ts
@@ -15,15 +15,36 @@ test('Launch the charts page and get values',async()=>{
         state: "visible"
     });
     
-    const parentelement = await page.locator('#line-composite-1', { has: page.locator('g.line-chart.chart-draw-area') });
-    const letcount = await parentelement.locator('g.dataset-units.dataset-line.dataset-0').locator('g');
-    const texts = await letcount.allTextContents();   
-    await console.log(`${texts}`);
    
 
- 
+    const parentelement = await page.locator('#line-composite-1', { has: page.locator('g.line-chart.chart-draw-area') });
+    const letcount = await parentelement.locator('g.dataset-units.dataset-line.dataset-0').locator('g');
+    const circletoclick = await parentelement.locator('g.dataset-units.dataset-line.dataset-0').locator('g >> circle');
+    const x_axis = await parentelement.locator("g.x.axis g");
 
+    const texts = await letcount.allTextContents();
+    const x_axis_text = await x_axis.allTextContents();
+    await console.log(`${texts}`);
+    await console.log(`${x_axis_text}`);  
 
+   const gettext = async () => {
+    const secondbarparent = await page.locator('#bar-composite-1',{has: page.locator('g.bar-chart.chart-draw-area')});
+    const totalbarcount = await secondbarparent.locator('g.dataset-units.dataset-bars.dataset-2').locator('g');
+    const bartext = await totalbarcount.allTextContents();
+    await console.log(`${bartext}`);
+   }
+
+const count2 = await circletoclick.count();
+for (let i =0;i< count2;i++){
+    await circletoclick.nth(i).click();
+   // await console.log(`${bartext}`);
+    await page.waitForTimeout(1000);
+    await gettext();
+    await page.waitForTimeout(1000);
+    
+}
   
 });
+
+           
 
